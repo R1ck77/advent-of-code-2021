327e2e1a54f644c0262fa821e4dbc725ab5a802b
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-01 14:28:33 +0100

    Add boilerplate for problem 1 part 1

---
+++ day1-test.el
@@ -0,0 +1,7 @@
+(require 'day1)
+(require 'buttercup)
+
+(describe "Day1 solution"
+  (it "replicates the part 1 example"
+    )
+  (it "solves the part 1"))
---
+++ day1.el
@@ -0,0 +1,10 @@
+(require 'dash)
+(require 'advent-utils)
+
+(defun day1-part-1 (input)
+  (error "Not implemented"))
+
+(defun day1-part-2 (input)
+  (error "Not implemented"))
+
+(provide 'day1)
