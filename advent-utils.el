d2ec5e98a44054e6fc2cbe921fea6f3173698dc1
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-01 15:08:43 +0100

    Some refactoring

--- advent-utils.el
+++ advent-utils.el
@@ -1,19 +1,19 @@
 
-(defun compute-input-name (day part type)
+(defun compute-input-name (day type)
   (format (cond
            ((eq type :example)
-            "data/day%d-example%d.txt")
+            "data/day%d-example.txt")
            ((eq type :problem)
-            "data/day%d-problem%d.txt")
+            "data/day%d-problem.txt")
            (t (error "Unexpected problem type")))
-          day part))
+          day))
 
-(defun read-problem-lines (day part type)
+(defun read-problem-lines (day type)
   (split-string (with-temp-buffer
-     (insert-file-contents (compute-input-name day part type))
+     (insert-file-contents (compute-input-name day type))
      (buffer-substring-no-properties (point-min) (point-max)))))
 
-(defun read-problem-numbers (day part type)
-  (-map #'string-to-number (read-problem-lines day part type)))
+(defun read-problem-numbers (day type)
+  (-map #'string-to-number (read-problem-lines day type)))
 
 (provide 'advent-utils)
--- day1-test.el
+++ day1-test.el
@@ -3,14 +3,14 @@
 
 (describe "Day 1 solution"
   (it "replicates the example of part 1"
-    (expect (day1/part-1 (read-problem-numbers 1 1 :example))
+    (expect (day1/part-1 (read-problem-numbers 1 :example))
             :to-be 7))
   (it "solves the first part of the problem"
-    (expect (day1/part-1 (read-problem-numbers 1 1 :problem))
+    (expect (day1/part-1 (read-problem-numbers 1 :problem))
             :to-be 1233))
   (it "replicates the example of part 2"
-    (expect (day1/part-2 (read-problem-numbers 1 1 :example))
+    (expect (day1/part-2 (read-problem-numbers 1 :example))
             :to-be 5))
   (it "solves the second part of the problem"
-    (expect (day1/part-2 (read-problem-numbers 1 1 :problem))
+    (expect (day1/part-2 (read-problem-numbers 1 :problem))
             :to-be 1275)))
