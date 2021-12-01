fa25f781c01571275a0b88dfcb9712b633f8d788
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-01 14:53:02 +0100

    Add boilerplate for day 1, part 2

--- day1-test.el
+++ day1-test.el
@@ -7,4 +7,10 @@
             :to-be 7))
   (it "solves the first part of the problem"
     (expect (day1/part-1 (read-problem-numbers 1 1 :problem))
-            :to-be 1233)))
+            :to-be 1233))
+  (it "replicates the example of part 2"
+    (expect (day1/part-2 (read-problem-numbers 1 1 :example))
+            :to-be 5))
+  (it "solves the second part of the problem"
+    (expect (day1/part-2 (read-problem-numbers 1 1 :problem))
+            :to-be 42)))
