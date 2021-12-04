cf80ac161383d523d2606389a6b18ef04e92fb55
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-04 12:30:26 +0100

    Day 4 part 1 solved

--- day4-test.el
+++ day4-test.el
@@ -6,10 +6,10 @@
     (it "replicates the example"
       (expect (day4/part-1 (advent/read-blocks-of-lines 4 :example))
               :to-be 4512 ))
-    (xit "solves the problem"
+    (it "solves the problem"
       (expect (day4/part-1 (advent/read-blocks-of-lines 4 :problem))
-              :to-be 3633500)))
-  (xdescribe "part 2"
+              :to-be 51776)))
+  (describe "part 2"
     (it "replicates the example"
       (expect (day4/part-2 (advent/read-blocks-of-lines 4 :example))
               :to-be 230))
--- day4.el
+++ day4.el
@@ -12,8 +12,50 @@
         (tables (-map #'day4/read-table (cdr blocks))))
     (list numbers tables)))
 
-(defun day4/part-1 (input)
-  (error "Not yet implemented"))
+(defun day4/transpose (lines)
+  (loop for column from 0 below (length lines)
+        collect (loop for line in lines
+                      collect (elt line column))))
+
+(defun day4/sum-remaining-values (rows)
+  "Given a list of rows (possibly null) return the sum of all values"
+  (apply #' + (--map (apply #'+ it) rows)))
+
+(defun day4/index-block (numbers lines)
+  "Index a block using the list of extractions
+
+Return the minimum number of extractions required to complete a
+line/column in a board, the last extracted number and the list of
+unmarked values.
+
+If the board cannot be completed with the extractions, return the
+number of extractions, the last value extracted, and the
+remaining values."
+  (let ((rows lines)
+        (columns (day4/transpose lines))
+        (number nil)
+        (extractions 0))
+    (while (and numbers
+                (not (-any #'null rows))
+                (not (-any #'null columns)))
+      (setq number (car numbers))
+      (setq extractions (1+ extractions))
+      (setq rows (--map (remove number it) rows))
+      (setq columns (--map (remove number it) columns))
+      (setq numbers (cdr numbers)))
+    (list extractions number rows)))
+
+(defun day4/index-blocks (problem)
+  (let ((extractions (car problem))
+        (tables (cadr problem)))
+    (--map (day4/index-block extractions it) tables)))
+
+(defun day4/part-1 (blocks)
+  (let ((problem (day4/read-problem blocks)))
+    (let ((indexed-solution (car (--sort (< (elt it 0)
+                                            (elt other 0))
+                                         (day4/index-blocks problem)))))
+      (* (elt indexed-solution 1) (day4/sum-remaining-values (elt indexed-solution 2))))))
 
 
 (defun day4/part-2 (input)
