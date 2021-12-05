3f0ec6860977ad1ea15330e9ee9f3083aa54f4af
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-05 15:19:29 +0100

    Add boilerplate for day 6

---
+++ day6-test.el
@@ -0,0 +1,18 @@
+(require 'day6)
+(require 'buttercup)
+
+(describe "Day 6"
+  (describe "part 1"
+    (it "replicates the example"
+      (expect (day6/part-1 (advent/read-problem-lines 6 :example))
+              :to-be 42 ))
+    (xit "solves the problem"
+      (expect (day6/part-1 (advent/read-problem-lines 6 :problem))
+              :to-be 42)))
+  (xdescribe "part 2"
+    (it "replicates the example"
+      (expect (day6/part-2 (advent/read-problem-lines 6 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day6/part-2 (advent/read-problem-lines 6 :problem))
+              :to-be 42))))
