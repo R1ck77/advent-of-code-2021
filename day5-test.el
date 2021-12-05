5543d625a97624860c9bcdd88d83875451d596d2
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-05 11:21:49 +0100

    Add day 5 data and example solution

--- day5-test.el
+++ day5-test.el
@@ -4,15 +4,15 @@
 (describe "Day 5"
   (describe "part 1"
     (it "replicates the example"
-      (expect (day5/part-1 (advent/read-blocks-of-lines 5 :example))
-              :to-be 42 ))
+      (expect (day5/part-1 (advent/read-problem-lines 5 :example))
+              :to-be 5 ))
     (xit "solves the problem"
-      (expect (day5/part-1 (advent/read-blocks-of-lines 5 :problem))
+      (expect (day5/part-1 (advent/read-problem-lines 5 :problem))
               :to-be 42)))
   (xdescribe "part 2"
     (it "replicates the example"
-      (expect (day5/part-2 (advent/read-blocks-of-lines 5 :example))
+      (expect (day5/part-2 (advent/read-problem-lines 5 :example))
               :to-be 42))
     (xit "solves the problem"
-      (expect (day5/part-2 (advent/read-blocks-of-lines 5 :problem))
+      (expect (day5/part-2 (advent/read-problem-lines 5 :problem))
               :to-be 42))))
