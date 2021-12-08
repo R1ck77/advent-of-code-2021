653869867071f2521307c96df10bf00216a31645
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-08 11:12:47 +0100

    Add boilerplate for day 8

---
+++ day8.el
@@ -0,0 +1,10 @@
+(require 'dash)
+(require 'advent-utils)
+
+(defun day8/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day8/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day8)
