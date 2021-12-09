ffd809635fe1b102361388980bac23ec32eef045
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-09 17:02:52 +0100

    Add boilerplate for day 10

---
+++ day10.el
@@ -0,0 +1,10 @@
+(require 'dash)
+(require 'advent-utils)
+
+(defun day10/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day10/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day10)
