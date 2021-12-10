a67abb4f47310b613da3451d3c50936fd92b96a0
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-10 09:24:52 +0100

    Add subtests to make my life easier

--- day10-test.el
+++ day10-test.el
@@ -3,6 +3,34 @@
 
 (describe "Day 10"
   (describe "part 1"
+    (describe "single tests"
+      (it "correctly categorizes [(()[<>])]({[<{<<[]>>( as not corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "[(()[<>])]({[<{<<[]>>("))
+                :to-be nil))
+      (it "correctly categorizes {([(<{}[<>[]}>{[]{[(<()> as corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "{([(<{}[<>[]}>{[]{[(<()>"))
+                :to-be :-c))
+      (it "correctly categorizes (((({<>}<{<{<>}{[]{[]{} as not corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "(((({<>}<{<{<>}{[]{[]{}"))
+                :to-be nil))
+      (it "correctly categorizes [[<[([]))<([[{}[[()]]] as corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "[[<[([]))<([[{}[[()]]]"))
+                :to-be :-r))
+      (it "correctly categorizes [{[{({}]{}}([{[{{{}}([] as corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "[{[{({}]{}}([{[{{{}}([]"))
+                :to-be :-s))
+      (it "correctly categorizes {<[[]]>}<{[{[{[]{()[[[] as not corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "{<[[]]>}<{[{[{[]{()[[[]"))
+                :to-be nil))
+      (it "correctly categorizes [<(<(<(<{}))><([]([]() as corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "[<(<(<(<{}))><([]([]()"))
+                :to-be :-r))
+      (it "correctly categorizes <{([([[(<>()){}]>(<<{{ as corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "<{([([[(<>()){}]>(<<{{"))
+                :to-be :-a))
+      (it "correctly categorizes <{([{{}}[<[[[<>{}]]]>[]] as not corrupted"
+        (expect (day10/corrupted? (day10/read-blocks "<{([{{}}[<[[[<>{}]]]>[]]"))
+                :to-be nil)))
     (it "replicates the example"
       (expect (day10/part-1 (advent/read-problem-lines 10 :example))
               :to-be 26397 ))
--- day10.el
+++ day10.el
@@ -1,6 +1,24 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defun day10/read-blocks (line)
+  (--map (case it
+           (?\( :+r)
+           (?\) :-r)
+           (?\[ :+s)
+           (?\] :-s)
+           (?\{ :+c)
+           (?\} :-c)
+           (?\< :+a)
+           (?\> :-a))
+         (-map #'string-to-char
+               (split-string line "" t))))
+
+(defun day10/read-instructions (lines)
+  (-map #'day10/read-blocks lines))
+
+(defun day10/corrupted? (blocks))
+
 (defun day10/part-1 (lines)
   (error "Not yet implemented"))
 
@@ -8,3 +26,4 @@
   (error "Not yet implemented"))
 
 (provide 'day10)
+
