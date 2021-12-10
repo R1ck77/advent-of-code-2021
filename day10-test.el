205afe249408f685800fe3f8e6a57e457ec62271
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-10 09:05:13 +0100

    Add day 10 data

--- day10-test.el
+++ day10-test.el
@@ -5,7 +5,7 @@
   (describe "part 1"
     (it "replicates the example"
       (expect (day10/part-1 (advent/read-problem-lines 10 :example))
-              :to-be 42 ))
+              :to-be 26397 ))
     (xit "solves the problem"
       (expect (day10/part-1 (advent/read-problem-lines 10 :problem))
               :to-be 42)))
