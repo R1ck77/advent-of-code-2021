73f60ad2bb60fa8ddf5ce694b52056cdd2527d15
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-11 12:16:17 +0100

    Day 11 part 1 solved

--- day11-test.el
+++ day11-test.el
@@ -3,12 +3,44 @@
 
 (describe "Day 11"
   (describe "part 1"
+    (describe "small example evolution"
+      (it "can replicate step 1"
+        (let ((input (day11/read-grid (split-string "11111
+19991
+19191
+19991
+11111"))))
+          (day11/evolve! input)
+          (expect (day11/str-grid input)
+                  :to-equal (day11/str-grid
+                             (day11/read-grid
+                              (split-string "34543
+40004
+50005
+40004
+34543"))))))
+
+      (it "can replicate step 2"
+        (let ((input (day11/read-grid (split-string "34543
+40004
+50005
+40004
+34543"))))
+          (day11/evolve! input)
+         (expect (day11/str-grid input)
+                 :to-equal (day11/str-grid
+                            (day11/read-grid
+                             (split-string "45654
+51115
+61116
+51115
+45654")))))))
     (it "replicates the example"
       (expect (day11/part-1 (advent/read-problem-lines 11 :example))
-              :to-be 42 ))
-    (xit "solves the problem"
+              :to-be 1656))
+    (it "solves the problem"
       (expect (day11/part-1 (advent/read-problem-lines 11 :problem))
-              :to-be 42)))
+              :to-be 1729)))
   (xdescribe "part 2"
     (it "replicates the example"
       (expect (day11/part-2 (advent/read-problem-lines 11 :example))
--- day11.el
+++ day11.el
@@ -1,8 +1,125 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defun day11/convert-cell (string-power)
+  (vector (string-to-number string-power) nil))
+
+(defun day11/convert-line (line)
+  (apply #'vector (-map #'day11/convert-cell (split-string line "" t))))
+
+(defmacro day11/loop-grid (grid &rest forms)
+  "Non-hygienic macro that bind all coordinates of the grid to 'it'"
+  (declare (indent 1))
+  (let ((rows (make-symbol "rows"))
+        (columns (make-symbol "columns"))
+        (i (make-symbol "i"))
+        (j (make-symbol "j"))
+        (it (make-symbol "it")))
+   `(let ((,rows (length ,grid))
+          (,columns (length (aref ,grid 0))))
+      (loop for ,i from 0 below ,rows do
+            (loop for ,j from 0 below ,columns do
+                  (let ((it (cons ,i ,j)))
+                    ,@forms))))))
+
+(defun day11/read-grid (lines)
+  (apply #'vector
+         (-map #'day11/convert-line
+               lines)))
+
+(defun day11/str-grid (grid)
+  (let ((result ""))
+    (loop for i from 0 below (length grid) do
+          (let ((row (aref grid i)))
+            (loop for j from 0 below (length row) do
+                  (let ((value (number-to-string (aref (aref row j) 0))))
+                    (setq result (concat result value))))
+            (setq result (concat result "\n"))))
+    (string-trim result)))
+
+(defun day11/increase-grid-energy-levels! (grid)
+  (day11/loop-grid grid
+    (let ((cell (aref (aref grid (car it)) (cdr it))))
+      (aset cell 0 (1+ (aref cell 0)))))
+  grid)
+
+(defun day11/get-cell (grid coord)
+  (aref (aref grid (car coord)) (cdr coord)))
+
+(defconst day11/neighbors '((-1 . -1) (-1 . 0) (-1 . 1)
+                            (0 . -1) (0 . 1)
+                            (1 . -1) (1 . 0) (1 . 1)))
+
+(defun day11/updated-coordinate (grid-size coord displacement)
+  (let ((new-row (+ (car coord)
+                    (car displacement))))
+    (if (and (>= new-row 0) (< new-row (car grid-size)))
+        (let ((new-column (+ (cdr coord)
+                             (cdr displacement))))
+          (and (>= new-column 0) (< new-column (cdr grid-size))
+               (cons new-row new-column))))))
+
+(defmacro day11/loop-neighbors (grid coord &rest body)
+  "Loop over all valid cell neighbors binding the coordinates to 'it'"
+  (declare (indent 2))
+  (let ((displacement (make-symbol "displacement"))
+        (grid-size (make-symbol "grid-size")))
+    `(let ((,grid-size (cons (length ,grid) (length (aref ,grid 0)))))
+       (-map (lambda (,displacement)
+               (let ((it (day11/updated-coordinate ,grid-size ,coord ,displacement)))
+                 (when it                     
+                   ,@body)))
+             day11/neighbors)
+       nil)))
+
+(defun day11/flash--increase! (grid coord)
+  "Increase (and possibly flash) all neighbors of coord"
+  (day11/loop-neighbors grid coord
+    (let ((current-cell (day11/get-cell grid it)))
+      ;; Increase the cell value regardless
+      (aset current-cell 0 (1+ (aref current-cell 0)))
+      ;; recur
+      (day11/flash--cell! grid it))))
+
+(defun day11/flash--cell! (grid coord)
+  (let ((cell (day11/get-cell grid coord)))
+    ;; if the cell didn't flash or the level is 9 or lower
+    (unless (or (aref cell 1)
+                (< (aref cell 0) 10))
+      ;; mark the cell as "flashed"
+      (aset cell 1 t)
+      ;; flash-increase all neighbors
+      (day11/flash--increase! grid coord))))
+
+(defun day11/flash--cycle! (grid)
+  "Make some octopi flash. Returns the modified grid"  
+  (day11/loop-grid grid
+    (day11/flash--cell! grid it)))
+
+(defun day11/clear-count-flashes! (grid)
+  (let ((counter 0))
+    (day11/loop-grid grid
+      (let ((cell (day11/get-cell grid it)))
+        (when (aref cell 1)
+          (setq counter (1+ counter))
+          (aset cell 1 nil)
+          (aset cell 0 0))
+        ;; make sure that at the end there are no unflashed cells with more than 9
+        (assert (< (aref cell 0) 10))))
+    counter))
+
+(defun day11/evolve! (grid)
+  "Evolve the grid and returns the number of flashes"
+  (day11/increase-grid-energy-levels! grid)
+  (day11/flash--cycle! grid)
+  (day11/clear-count-flashes! grid))
+
 (defun day11/part-1 (lines)
-  (error "Not yet implemented"))
+  (let ((grid (day11/read-grid lines))
+        (counter 0))
+    (loop repeat 100 do
+          (setq counter (+ counter (day11/evolve! grid))))
+    counter))
 
 (defun day11/part-2 (lines)
   (error "Not yet implemented"))
