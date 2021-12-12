03e89bea3939ea35024aa5058298cdffe5a6d7ae
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-12 12:37:23 +0100

    Add auxiliary functions for day 12

--- day12-test.el
+++ day12-test.el
@@ -4,39 +4,42 @@
 (describe "Day 12"
   (describe "part 1"
     (describe "small examples"
-      (it "solves the small example"
-        (expect (day12/part-1 "start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-end")
-                :to-be 10))
-      (it "generates the same paths for the small example"
-        (expect (sort
-                 (-map #'day12/str-path
-                       (day12/compute-all-paths "start-A
+      (it "computes the number of paths for the tiny example"
+        (expect (day12/part-1
+                 (split-string "start-A
 start-b
 A-c
 A-b
 b-d
 A-end
 b-end"))
+                :to-be 10))
+      (it "computes all paths for the tiny example"
+        (expect (sort
+                 (-map #'day12/str-path
+                       (day12/compute-all-paths
+                        (split-string "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end")))
                  #'string<)
-                :to-be (sort ("start,A,b,A,c,A,end"
-                              "start,A,b,A,end"
-                              "start,A,b,end"
-                              "start,A,c,A,b,A,end"
-                              "start,A,c,A,b,end"
-                              "start,A,c,A,end"
-                              "start,A,end"
-                              "start,b,A,c,A,end"
-                              "start,b,A,end"
-                              "start,b,end")
-                             #'string<)))
-      (xit "solves the medium example"
-        (expect (day12/part-1 "dc-end
+                :to-equal (sort '("start,A,b,A,c,A,end"
+                                  "start,A,b,A,end"
+                                  "start,A,b,end"
+                                  "start,A,c,A,b,A,end"
+                                  "start,A,c,A,b,end"
+                                  "start,A,c,A,end"
+                                  "start,A,end"
+                                  "start,b,A,c,A,end"
+                                  "start,b,A,end"
+                                  "start,b,end")
+                                #'string<)))
+      (xit "computes the number of paths for the medium example"
+        (expect (day12/part-1
+                 (split-string "dc-end
 HN-start
 start-kj
 dc-start
@@ -45,7 +48,7 @@ LN-dc
 HN-end
 kj-sa
 kj-HN
-kj-dc")
+kj-dc"))
                 :to-be 19)))
     (xit "replicates the example"
       (expect (day12/part-1 (advent/read-problem-lines 12 :example))
--- day12.el
+++ day12.el
@@ -1,8 +1,38 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defun day12/node-to-keyword (node-name)
+  (intern (concat ":" node-name)))
+
+(defun day12/keyword-to-node (node)
+  (substring (symbol-name node) 1))
+
+(defun day12/add--leg (connections pair)
+  (let ((destinations (advent/get connections (car pair) nil)))
+    (advent/put connections (car pair) (cons (cadr pair) destinations))))
+
+(defun day12/add-connection (connections pair)
+  (day12/add--leg connections pair)
+  (day12/add--leg connections (reverse pair))
+  connections)
+
+(defun day12/parse-connection (connections line)
+  (let ((new-connection (-map #'day12/node-to-keyword (split-string line "-"))))
+    (day12/add-connection (or connections (advent/table)) new-connection)))
+
+(defun day12/read-nodes (lines)
+  (-reduce-from #'day12/parse-connection nil lines))
+
+(defun day12/str-path (path)
+  (apply #'concat (-interpose "," (-map #'day12/keyword-to-node path))))
+
+(defun day12/compute-all-paths (connections)
+  nil)
+
 (defun day12/part-1 (lines)
-  (error "Not yet implemented"))
+  (length
+   (day12/compute-all-paths
+    (day12/read-nodes lines))))
 
 (defun day12/part-2 (lines)
   (error "Not yet implemented"))
