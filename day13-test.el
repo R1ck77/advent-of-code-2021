d25ebfd6cdabe1a6a870aa7443909bcaf8a559bb
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-13 12:32:21 +0100

    Day 13 solved

--- day13-test.el
+++ day13-test.el
@@ -5,24 +5,38 @@
   (describe "part 1"
     (describe "vertical fold works"
       (it "replicates the first fold"
-       (expect (day13/format-paper (day13/fold (advent/read-blocks-of-lines 13 :example)))
+       (expect (day13/format-paper (day13/fold (day13/read-state (advent/read-blocks-of-lines 13 :example))))
                :to-equal "#.##..#..#.
 #...#......
 ......#...#
 #...#......
 .#.#..#.###
 ...........
-...........")))
-    (xit "replicates the example"
+...........
+")))
+    (it "replicates the example"
       (expect (day13/part-1 (advent/read-blocks-of-lines 13 :example))
               :to-be 17 ))
-    (xit "solves the problem"
+    (it "solves the problem"
       (expect (day13/part-1 (advent/read-blocks-of-lines 13 :problem))
-              :to-be 42)))
-  (xdescribe "part 2"
+              :to-be 621)))
+  (describe "part 2"
     (it "replicates the example"
       (expect (day13/part-2 (advent/read-blocks-of-lines 13 :example))
-              :to-be 42))
-    (xit "solves the problem"
+              :to-equal "#####
+#...#
+#...#
+#...#
+#####
+.....
+.....
+"))
+    (it "solves the problem"
       (expect (day13/part-2 (advent/read-blocks-of-lines 13 :problem))
-              :to-be 42))))
+              :to-equal "#..#.#..#.#..#...##..##...##....##.####.
+#..#.#.#..#..#....#.#..#.#..#....#....#.
+####.##...#..#....#.#....#..#....#...#..
+#..#.#.#..#..#....#.#.##.####....#..#...
+#..#.#.#..#..#.#..#.#..#.#..#.#..#.#....
+#..#.#..#..##...##...###.#..#..##..####.
+"))))
--- day13.el
+++ day13.el
@@ -9,26 +9,17 @@
 (defun day13/read-points-coordinates (lines)
   (-map #'day13/read-coordinate lines))
 
-(defun day13/compute-grid-size (points)
-  (let ((further-point (--reduce-from (cons (max (car it) (car acc))
-                              (max (cdr it) (cdr acc)))
-                        '(0 . 0)
-                        points )))
-    (cons (1+ (car further-point))
-          (1+ (cdr further-point)))))
-
 (defun day13/create-empty-paper (width-height)  
   (apply #'vector
          (--map (make-vector (car width-height) nil)
                 (number-sequence 0 (1- (cdr width-height))))))
 
-(defun day13/read-paper (lines)
-  (let ((points (day13/read-points-coordinates lines)))
-    (let ((grid (day13/create-empty-paper (day13/compute-grid-size points))))
-      (--each points
-        (let ((row (aref grid (cdr it))))
-          (aset row (car it) t)))
-      grid)))
+(defun day13/read-paper (grid-size lines)
+  (let ((grid (day13/create-empty-paper grid-size)))
+    (--each (day13/read-points-coordinates lines)
+      (let ((row (aref grid (cdr it))))
+        (aset row (car it) t)))
+    grid))
 
 (defun day13/read-fold (line)
   (let ((tokens (split-string line " " t)))
@@ -41,9 +32,17 @@
 (defun day13/read-folds (lines)
   (-map #'day13/read-fold lines))
 
+(defun day13/compute-grid-size (folds)
+  (let ((first-x (caar (--filter (not (zerop (car it))) folds)))
+        (first-y (cdar (--filter (not (zerop (cdr it))) folds))))
+    (cons (1+ (* first-x 2))
+          (1+ (* first-y 2)))))
+
 (defun day13/read-state (blocks)
-  (cons (day13/read-paper (car blocks))
-        (day13/read-folds (cadr blocks))))
+  (let ((folds (day13/read-folds (cadr blocks))))
+    (cons (day13/read-paper (day13/compute-grid-size folds)
+                            (car blocks))
+          folds)))
 
 (defun day13/get (state column-row)
   (aref (aref (car state) (cdr column-row)) (car column-row)))
@@ -51,8 +50,77 @@
 (defun day13/reverse-get (state row-column)
   (day13/get state (cons (cdr row-column) (car row-column))))
 
+(defun day13/cut-paper-y (old-paper y)
+  "Cut paper along x on the specified row"
+  (assert (= (1+ (* y 2)) (length old-paper)))
+  (let ((paper-columns (length (aref old-paper 0))))
+    (let ((new-paper (day13/create-empty-paper (cons paper-columns y))))
+      (loop for i from 0 below y do
+            (let ((old-row (aref old-paper i))
+                  (new-row (aref new-paper i)))
+              (loop for j from 0 below paper-columns do
+                    (aset new-row j (aref old-row j)))))
+      new-paper)))
+
+(defun day13/mirror-y! (old-paper new-paper y)
+  (let ((paper-columns (length (aref old-paper 0)))
+        (old-paper-rows (length old-paper)))
+    (loop for i from 0 below y do
+          (let ((new-row (aref new-paper i))
+                (old-row (aref old-paper (- old-paper-rows i 1))))
+            (loop for j from 0 below paper-columns do                
+                  (aset new-row j (or (aref new-row j)
+                                      (aref old-row j))))))
+    new-paper))
+
+(defun day13/fold-y (old-paper coord)
+  (let* ((y (cdr coord))
+        (new-paper (day13/cut-paper-y old-paper y)))
+    (day13/mirror-y! old-paper new-paper y)
+    new-paper))
+
+(defun day13/cut-paper-x (old-paper x)
+  "Cut paper along y on the specified column"
+  (assert (= (1+ (* x 2)) (length (aref old-paper 0))))
+  (let ((paper-rows (length old-paper)))
+    (let ((new-paper (day13/create-empty-paper (cons x paper-rows))))
+      (loop for i from 0 below paper-rows do
+            (let ((old-row (aref old-paper i))
+                  (new-row (aref new-paper i)))
+              (loop for j from 0 below x do
+                    (aset new-row j (aref old-row j)))))
+      new-paper)))
+
+(defun day13/mirror-x! (old-paper new-paper x)
+  (let ((paper-rows (length old-paper))
+        (old-paper-columns (length (aref old-paper 0))))
+    (loop for i from 0 below paper-rows do
+          (let ((new-row (aref new-paper i))
+                (old-row (aref old-paper i)))
+            (loop for j from 0 below x do                
+                  (aset new-row j (or (aref new-row j)
+                                      (aref old-row (- old-paper-columns j 1)))))))
+    new-paper))
+
+(defun day13/fold-x (old-paper coord)
+  (let* ((x (car coord))
+        (new-paper (day13/cut-paper-x old-paper x)))
+    (day13/mirror-x! old-paper new-paper x)
+    new-paper))
+
 (defun day13/fold (state)
-  state)
+  (let ((paper (car  state))
+        (folds (cdr state)))
+    (let ((current-fold (car folds)))      
+      (cons (funcall (if (zerop (car current-fold))
+                         #'day13/fold-y
+                       #'day13/fold-x)
+                     paper
+                     current-fold)
+            (cdr folds)))))
+
+(defun day13/fold-all (state)
+  (--reduce-from (day13/fold acc) state (cdr state)))
 
 (defun day13/format-paper (state)
   (let ((result "")
@@ -79,9 +147,9 @@
     counter))
 
 (defun day13/part-1 (lines)
-  (error "Not yet implemented"))
+  (day13/count-marks (day13/fold (day13/read-state lines))))
 
 (defun day13/part-2 (lines)
-  (error "Not yet implemented"))
+  (day13/format-paper (day13/fold-all (day13/read-state lines))))
 
 (provide 'day13)
