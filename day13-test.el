ecf1d224e58419e369d1cd6efcf4fff2721323f5
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-13 11:21:41 +0100

    Add code to read the input

--- day13-test.el
+++ day13-test.el
@@ -3,16 +3,26 @@
 
 (describe "Day 13"
   (describe "part 1"
-    (it "replicates the example"
-      (expect (day13/part-1 (advent/read-problem-lines 13 :example))
-              :to-be 42 ))
+    (describe "vertical fold works"
+      (it "replicates the first fold"
+       (expect (day13/format-paper (day13/fold (advent/read-blocks-of-lines 13 :example)))
+               :to-equal "#.##..#..#.
+#...#......
+......#...#
+#...#......
+.#.#..#.###
+...........
+...........")))
+    (xit "replicates the example"
+      (expect (day13/part-1 (advent/read-blocks-of-lines 13 :example))
+              :to-be 17 ))
     (xit "solves the problem"
-      (expect (day13/part-1 (advent/read-problem-lines 13 :problem))
+      (expect (day13/part-1 (advent/read-blocks-of-lines 13 :problem))
               :to-be 42)))
   (xdescribe "part 2"
     (it "replicates the example"
-      (expect (day13/part-2 (advent/read-problem-lines 13 :example))
+      (expect (day13/part-2 (advent/read-blocks-of-lines 13 :example))
               :to-be 42))
     (xit "solves the problem"
-      (expect (day13/part-2 (advent/read-problem-lines 13 :problem))
+      (expect (day13/part-2 (advent/read-blocks-of-lines 13 :problem))
               :to-be 42))))
--- day13.el
+++ day13.el
@@ -1,6 +1,83 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defun day13/read-coordinate (line)
+  (let ((as-list (-map #'string-to-number
+                       (split-string line "," t))))
+    (cons (car as-list) (cadr as-list))))
+
+(defun day13/read-points-coordinates (lines)
+  (-map #'day13/read-coordinate lines))
+
+(defun day13/compute-grid-size (points)
+  (let ((further-point (--reduce-from (cons (max (car it) (car acc))
+                              (max (cdr it) (cdr acc)))
+                        '(0 . 0)
+                        points )))
+    (cons (1+ (car further-point))
+          (1+ (cdr further-point)))))
+
+(defun day13/create-empty-paper (width-height)  
+  (apply #'vector
+         (--map (make-vector (car width-height) nil)
+                (number-sequence 0 (1- (cdr width-height))))))
+
+(defun day13/read-paper (lines)
+  (let ((points (day13/read-points-coordinates lines)))
+    (let ((grid (day13/create-empty-paper (day13/compute-grid-size points))))
+      (--each points
+        (let ((row (aref grid (cdr it))))
+          (aset row (car it) t)))
+      grid)))
+
+(defun day13/read-fold (line)
+  (let ((tokens (split-string line " " t)))
+    (assert (string= (car tokens) "fold"))
+    (let ((raw (split-string (elt tokens 2)  "=" t)))
+      (if (string= (car raw) "x")
+          (cons (string-to-number (cadr raw)) 0)
+        (cons 0 (string-to-number (cadr raw)))))))
+
+(defun day13/read-folds (lines)
+  (-map #'day13/read-fold lines))
+
+(defun day13/read-state (blocks)
+  (cons (day13/read-paper (car blocks))
+        (day13/read-folds (cadr blocks))))
+
+(defun day13/get (state column-row)
+  (aref (aref (car state) (cdr column-row)) (car column-row)))
+
+(defun day13/reverse-get (state row-column)
+  (day13/get state (cons (cdr row-column) (car row-column))))
+
+(defun day13/fold (state)
+  state)
+
+(defun day13/format-paper (state)
+  (let ((result "")
+        (paper (car state)))
+    (loop for i
+          from 0
+          below (length paper)
+          do
+          (loop for j
+                from 0
+                below (length (aref paper 0))
+                do
+                (setq result (concat result (if (day13/get state (cons j i)) "#" ".")))
+                )
+          (setq result (concat result "\n")))
+    result))
+
+(defun day13/count-marks (state)
+  (let ((counter 0))
+    (advent/loop-grid (car state)
+      (let ((row-column it))
+        (when (day13/reverse-get state it)
+          (setq counter (1+ counter)))))
+    counter))
+
 (defun day13/part-1 (lines)
   (error "Not yet implemented"))
 
