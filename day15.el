7b26411f8b9f5b5c8535b8c847624f011f5b4137
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-14 18:21:56 +0100

    Add boilerplate for day 15

---
+++ day15.el
@@ -0,0 +1,10 @@
+(require 'dash)
+(require 'advent-utils)
+
+(defun day15/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day15/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day15)
