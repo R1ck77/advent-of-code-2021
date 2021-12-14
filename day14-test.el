aa860fd0050d22a64d97ddb0a4a4db7cb32da59f
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-14 18:20:59 +0100

    Read day 14 problem

--- day14-test.el
+++ day14-test.el
@@ -1,28 +1,29 @@
 (require 'day14)
 (require 'buttercup)
 
+
 (describe "Day 14"
   (describe "part 1"
     (describe "expansion"
       (it "expands the example correctly once"
         (expect (day14/format-polymer
                  (day14/expand
-                  (day14/read-polymer "NNCB")))
+                  (day14/read-template (advent/read-blocks-of-lines 14 :example))))
                 :to-equal "NCNBCHB"))
       (it "expands the example correctly twice"
         (expect (day14/format-polymer
                  (day14/expand
-                  (day14/read-polymer "NNCB") 2))
+                  (day14/read-template (advent/read-blocks-of-lines 14 :example)) 2))
                 :to-equal "NBCCNBBBCBHCB"))
       (it "expands the example correctly three times"
         (expect (day14/format-polymer
                  (day14/expand
-                  (day14/read-polymer "NNCB") 3))
+                  (day14/read-template (advent/read-blocks-of-lines 14 :example)) 3))
                 :to-equal "NBBBCNCCNBBNBNBBCHBHHBCHB"))
       (it "expands the example correctly four times"
         (expect (day14/format-polymer
                  (day14/expand
-                  (day14/read-polymer "NNCB") 4))
+                  (day14/read-template (advent/read-blocks-of-lines 14 :example)) 4))
                 :to-equal "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")))
     (it "replicates the example"
       (expect (day14/part-1 (advent/read-blocks-of-lines 14 :example))
--- day14.el
+++ day14.el
@@ -1,10 +1,60 @@
 (require 'dash)
 (require 'advent-utils)
 
-(defun day14/part-1 (lines)
+(defun day14/letter-to-token (letter)
+  (intern (concat ":" letter)))
+
+(defun day14/token-to-letter (token)
+  (substring (symbol-name) 1))
+
+(defun day14/read-polymer (line)
+  (let ((all-tokens (-map #'day14/letter-to-token
+                          (split-string line "" t))))
+    (-partition-in-steps 2 1 all-tokens)))
+
+(defun day14/read-rule (line)
+  "Turns NC -> H into ((:N :C) ((:N :H) (:C :H))"
+  (let* ((tokens (split-string line))
+         (starting (car (day14/read-polymer (car tokens))))
+         (new-token (day14/letter-to-token (elt tokens 2))))
+    (list starting (list (list (car starting) new-token)
+                         (list new-token (cadr starting))))))
+
+(defun day14/debug--print-rules (template)  
+  (let ((rules (cadr template))
+        (result ""))
+    (maphash (lambda (k v)
+               (setq result
+                     (concat result (format "%s -> %s\n" k v))))
+             rules)
+    result))
+
+(defun day14/read-rules (lines)
+  (let ((rules (advent/table)))
+    (--each (-map #'day14/read-rule lines)
+      (advent/put rules (car it) (cadr it)))
+    rules))
+
+(defun day14/read-template (blocks)
+  (let ((polymer (day14/read-polymer (caar blocks)))
+        (rules (day14/read-rules (cadr blocks))))
+    (list polymer rules)))
+
+(defun day14/expand (template times)
+  template)
+
+(defun day14/format-polymer (template)
+  (let* ((polymer (car template))
+         (all-but-last (apply #'day14/token-to-letter (-map #'car polymer)))
+         (last-pair (car (cdr (car (last polymer))))))
+   ))
+
+(defun day14/part-1 (blocks)
+  (defun exp(day14/read-template lines))
   (error "Not yet implemented"))
 
-(defun day14/part-2 (lines)
+(defun day14/part-2 (blocks)
   (error "Not yet implemented"))
 
 (provide 'day14)
+
