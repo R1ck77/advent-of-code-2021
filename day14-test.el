fbc490b4033bcefce99083238d9bea9ff1d2c17e
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-14 09:57:09 +0100

    Add boilerplate code for day 14

---
+++ day14-test.el
@@ -0,0 +1,39 @@
+(require 'day14)
+(require 'buttercup)
+
+(describe "Day 14"
+  (describe "part 1"
+    (describe "expansion"
+      (it "expands the example correctly once"
+        (expect (day14/format-polymer
+                 (day14/expand
+                  (day14/read-polymer "NNCB")))
+                :to-equal "NCNBCHB"))
+      (it "expands the example correctly twice"
+        (expect (day14/format-polymer
+                 (day14/expand
+                  (day14/read-polymer "NNCB") 2))
+                :to-equal "NBCCNBBBCBHCB"))
+      (it "expands the example correctly three times"
+        (expect (day14/format-polymer
+                 (day14/expand
+                  (day14/read-polymer "NNCB") 3))
+                :to-equal "NBBBCNCCNBBNBNBBCHBHHBCHB"))
+      (it "expands the example correctly four times"
+        (expect (day14/format-polymer
+                 (day14/expand
+                  (day14/read-polymer "NNCB") 4))
+                :to-equal "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")))
+    (it "replicates the example"
+      (expect (day14/part-1 (advent/read-blocks-of-lines 14 :example))
+              :to-be 1588 ))
+    (xit "solves the problem"
+      (expect (day14/part-1 (advent/read-blocks-of-lines 14 :problem))
+              :to-be 42)))
+  (xdescribe "part 2"
+    (it "replicates the example"
+      (expect (day14/part-2 (advent/read-blocks-of-lines 14 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day14/part-2 (advent/read-blocks-of-lines 14 :problem))
+              :to-be 42))))
---
+++ day14.el
@@ -0,0 +1,10 @@
+(require 'dash)
+(require 'advent-utils)
+
+(defun day14/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day14/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day14)
