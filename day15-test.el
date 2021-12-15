c38f424fd03a173630cf5cdb52df70bfbb2237a6
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-15 11:30:33 +0100

    Add data and test for part 1

--- day15-test.el
+++ day15-test.el
@@ -5,7 +5,7 @@
   (describe "part 1"
     (it "replicates the example"
       (expect (day15/part-1 (advent/read-problem-lines 15 :example))
-              :to-be 42 ))
+              :to-be 40 ))
     (xit "solves the problem"
       (expect (day15/part-1 (advent/read-problem-lines 15 :problem))
               :to-be 42)))
