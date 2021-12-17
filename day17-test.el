832529ea36eac268cd9f42973fd30f4c21ae2de6
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-17 15:25:51 +0100

    Day 17 part 1 solved

--- day17-test.el
+++ day17-test.el
@@ -6,9 +6,9 @@
     (it "replicates the example"
       (expect (day17/part-1 (car (advent/read-problem-lines 17 :example)))
               :to-be 45 ))
-    (xit "solves the problem"
+    (it "solves the problem"
       (expect (day17/part-1 (car (advent/read-problem-lines 17 :problem)))
-              :to-be 42)))
+              :to-be 3160)))
   (xdescribe "part 2"
     (it "replicates the example"
       (expect (day17/part-2 (car (advent/read-problem-lines 17 :example)))
--- day17.el
+++ day17.el
@@ -1,6 +1,8 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defconst max-vy-search 1001)
+
 (defun day17/read-target (line)
   (string-match "target area: x=\\([-0-9]+\\)\.\.\\([-0-9]+\\), y=\\([-0-9]+\\)\.\.\\([-0-9]+\\)" line)
   (list :x-min (string-to-number (match-string 1 line))
@@ -8,13 +10,35 @@
         :y-min (string-to-number (match-string 3 line))
         :y-max (string-to-number (match-string 4 line))))
 
+
+(defun day17/vertical-hit? (target vy)
+  "Returns the maximum-position if the probe hits the mark, nil otherwise"
+  (let ((overshot)
+        (hit)
+        (pos 0)
+        (max-pos 0))
+    (while (not (or overshot hit))
+      (setq pos (+ pos vy))
+      (setq max-pos (max max-pos pos))
+      (setq vy (1- vy))
+      (setq overshot (< pos (plist-get target :y-min)))
+      (setq hit (and (not overshot)
+                     (<= pos (plist-get target :y-max)))))
+    (and (not overshot) (list vy max-pos))))
+
+(defun day17/compute-max-vy (target top-vy)
+  (cadr (car (nreverse (-non-nil 
+                   (--map (day17/vertical-hit? target it)
+                          (number-sequence 0 (or top-vy 1001))))))))
+
 (defun day17/part-1 (line)
-  (error "Not yet implemented"))
+  "1001 is quite arbitrary: I have no idea whether a solution exists for vy = 1e39. Is there a mathematical proof?"
+  (day17/compute-max-vy (day17/read-target line) max-vy-search))
 
 (defun day17/part-2 (line)
   (error "Not yet implemented"))
 
 (provide 'day17)
 
-(setq example "target area: x=20..30, y=-10..-5")
-(setq problem "target area: x=282..314, y=-80..-45")
+(setq example (day17/read-target "target area: x=20..30, y=-10..-5"))
+(setq problem (day17/read-target "target area: x=282..314, y=-80..-45"))
