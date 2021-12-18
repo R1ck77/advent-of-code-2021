7b0b09aa5e839a6f5bdf4884bc7c7a24d298063a
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-18 11:11:46 +0100

    Read expressions correctly

--- day18.el
+++ day18.el
@@ -1,7 +1,15 @@
 (require 'dash)
+(require 's)
 (require 'advent-utils)
 
+(defun day18/read-number (line)
+  (let ((clean-text (s-replace "," " "
+                               (s-replace "]" ")"
+                                          (s-replace "[" "(" line)))))
+    (eval (car (read-from-string (concat "'" clean-text))))))
+
 (defun day18/part-1 (lines)
+  (-map #'day18/read-number lines)
   (error "Not yet implemented"))
 
 (defun day18/part-2 (lines)
