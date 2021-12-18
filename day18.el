e34df96dadba922b9ea73e5760264ddb81cd8d49
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-18 11:50:21 +0100

    Add magnitude and WIP to explosion

--- day18.el
+++ day18.el
@@ -8,9 +8,31 @@
                                           (s-replace "[" "(" line)))))
     (eval (car (read-from-string (concat "'" clean-text))))))
 
-(defun day18/explode-1 (number)
-  ;;TODO/FIXME
+(defun day18/recur-until-equal (value f)
+  (let ((transformed value))
+    (setq value nil)
+    (while (not (equal transformed value))
+      (setq value transformed)
+      (setq transformed (funcall f value)))
+    transformed))
+
+(defun day18/explode-left (number)
   number)
+
+(defun day18/reverse (number)
+  (if (numberp number)
+      number
+    (list (day18/reverse (cadr number))
+          (day18/reverse (car number)))))
+
+(defun day18/explode-right (number)
+  (day18/reverse (day18/explode-left (day18/reverse number))))
+
+(defun day18/explode-1 (number)
+  (day18/explode-right (day18/explode-left number)))
+
+(defun day18/explode (number)
+  (day18/recur-until-equal number #'day18-explode-1))
 
 (defun day18/split-number (number)
   (let ((remainder (mod number 2))
@@ -24,16 +46,24 @@
         value
       (day18/split-number value))))
 
+(defun day18/reduce-1 (number)
+  (day18/split (day18/explode number)))
+
 (defun day18/reduce (number)
-  ;;TODO/FIXME
-  number)
+  (day18/recur-until-equal number #'day18/reduce-1))
 
 (defun day18/sum-all (numbers)
   (day18/reduce (--reduce (list acc it) numbers)))
 
+(defun day18/magnitude (number)
+  (if (numberp number)
+      number
+    (+ (* 3 (day18/magnitude (car number)))
+       (* 2 (day18/magnitude (cadr number))))))
+
 (defun day18/part-1 (lines)
-  (-map #'day18/read-number lines)
-  (error "Not yet implemented"))
+  (day18/magnidue
+   (day18/sum-all (-map #'day18/read-number lines))))
 
 (defun day18/part-2 (lines)
   (error "Not yet implemented"))
