07f46a5ce382c084f7e42be71c64edbd39cacf77
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-19 13:36:05 +0100

    Add a transform function

--- day19.el
+++ day19.el
@@ -28,6 +28,50 @@
                             (lambda (x y z) (list (- y) (- z) x)))
   "List of all spatial transformations, identities included")
 
+(defun day19/visible-beacon? (point)
+;;; TODO/FIXME bland optimization possible
+  (let ((x (car point))
+        (y (elt point 1))
+        (z (elt point 2)))
+    (and (>= x -1000)
+         (<= x 1000)
+         (>= y -1000)
+         (<= y 1000)
+         (>= z 1000)
+         (<= z 1000))))
+
+(defun day19/two-point-match-for-translation? (ref other translation))
+
+(defun day19/create-translation (dest src)
+  "Creates a transform that turns src point into dst points
+
+Can be used to move a point from the 'other' set to the 'reference' set"
+  (lexical-let ((dx (- (car dest) (car src)))
+                (dy (- (elt dest 1) (elt src 1)))
+                (dz (- (elt dest 2) (elt src 2))))
+    (lambda (x y z)
+      (list (+ x dx)
+            (+ y dy)
+            (+ z dz)))))
+
+(defun day19/two--point--match? (ref other)
+  "Given the two groups of beacons in the same orientation, return the list of transforms with *at least* a two point match
+
+This is a preliminary check, if a transform works here, it's worth re-checking more thoroughly"
+  (let ((destination-points ref)
+        (candidates)))
+  ;; for each point of the reference set
+  (while destination-points
+    (let ((dest-point (pop translations))
+          (source-points other))
+      ;; for each point of the other set
+      (while source-points
+        (let ((translation (day19/create-translation dest-point (pop source-point))))
+          ;; if there is at least a 2 point match, add it to the 
+          (when (day19/two-point-match-for-translation? ref other translation)
+            (push translation candidates))))))
+  candidates)
+
 (defun day19/read-coordinate (line)
   (-map #'string-to-number
         (split-string line "," t)))
