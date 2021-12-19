c78efb934c498cebdf5299acecee3ab319f6e246
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-19 11:47:40 +0100

    Read data for day 19

--- day19.el
+++ day19.el
@@ -1,6 +1,20 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defun day19/read-coordinate (line)
+  (-map #'string-to-number
+        (split-string line "," t)))
+
+(defun day19/read-title (line)
+  (string-to-number (elt (split-string line " " t) 2)))
+
+(defun day19/read-beacon (lines)
+  ;;(print (format "Reading probe '%d'" (day19/read-title (car lines))))
+  (-map #'day19/read-coordinate (cdr lines)))
+
+(defun day19/read-beacons (blocks)
+  (-map #'day19/read-beacon blocks))
+
 (defun day19/part-1 (lines)
   (error "Not yet implemented"))
 
