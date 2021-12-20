f2f6506d39ec57deb6c8b7c152ce2aaa93e19fa2
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-20 01:07:33 +0100

    Day 19 solved

--- day19-test.el
+++ day19-test.el
@@ -5,54 +5,68 @@
 
 (describe "Day 19"
   (describe "part 1"
-    (xdescribe "scan pairs detection"
+    (describe "scan pairs detection"
       (it "can detect the pairing of 1 and 0"
-        (expect (day19/two-scanners-match? (elt day19-test/example 0)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 0)
                                            (elt day19-test/example 1))
                :not :to-be nil)
-        (expect (day19/two-scanners-match? (elt day19-test/example 1)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 1)
                                            (elt day19-test/example 0))
                :not :to-be nil))
       (it "can detect the pairing of 1 and 4"
-        (expect (day19/two-scanners-match? (elt day19-test/example 4)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 4)
                                            (elt day19-test/example 1))
                :not :to-be nil)
-        (expect (day19/two-scanners-match? (elt day19-test/example 1)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 1)
                                            (elt day19-test/example 4))
                 :not :to-be nil))
       (it "can detect the pairing of 4 and 2"
-        (expect (day19/two-scanners-match? (elt day19-test/example 4)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 4)
                                            (elt day19-test/example 2))
                :not :to-be nil)
-        (expect (day19/two-scanners-match? (elt day19-test/example 2)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 2)
                                            (elt day19-test/example 4))
                 :not :to-be nil))
       (it "can detect the pairing of 1 and 3"
-        (expect (day19/two-scanners-match? (elt day19-test/example 1)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 1)
                                            (elt day19-test/example 3))
                :not :to-be nil)
-        (expect (day19/two-scanners-match? (elt day19-test/example 3)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 3)
                                            (elt day19-test/example 1))
                :not :to-be nil))      
       (it "can detect the lack of pairing between 3 and 4"
-        (expect (day19/two-scanners-match? (elt day19-test/example 3)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 3)
                                            (elt day19-test/example 4))
                :to-be nil)
-        (expect (day19/two-scanners-match? (elt day19-test/example 3)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 3)
                                            (elt day19-test/example 4))
                 :to-be nil))
       (it "can detect the lack of pairing between 1 and 2"
-        (expect (day19/two-scanners-match? (elt day19-test/example 1)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 1)
                                            (elt day19-test/example 2))
                :to-be nil)
-        (expect (day19/two-scanners-match? (elt day19-test/example 2)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 2)
                                            (elt day19-test/example 1))
                 :to-be nil))
       (it "can detect the lack of pairing between 4 and 3"
-        (expect (day19/two-scanners-match? (elt day19-test/example 4)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 4)
                                            (elt day19-test/example 3))
                :to-be nil)
-        (expect (day19/two-scanners-match? (elt day19-test/example 3)
+        (expect (day19/two-scanners-match? '(0 0 0)
+                                           (elt day19-test/example 3)
                                            (elt day19-test/example 4))
                :to-be nil)))
     (it "replicates the example"
@@ -60,11 +74,11 @@
               :to-be 79 ))
     (it "solves the problem"
       (expect (day19/part-1 (advent/read-blocks-of-lines 19 :problem))
-              :to-be 42)))
-  (xdescribe "part 2"
+              :to-be 396)))
+  (describe "part 2"
     (it "replicates the example"
       (expect (day19/part-2 (advent/read-blocks-of-lines 19 :example))
-              :to-be 42))
-    (xit "solves the problem"
+              :to-be 3621))
+    (it "solves the problem"
       (expect (day19/part-2 (advent/read-blocks-of-lines 19 :problem))
-              :to-be 42))))
+              :to-be 11828))))
--- day19.el
+++ day19.el
@@ -28,8 +28,8 @@
   "List of all spatial transformations, identity included")
 
 (defun day19/debug-print (value)
-  (print value)
-  (redisplay))
+  (comment (print value)
+           (redisplay)))
 
 (defun day19/count-matches (group1 group2)
   (if (> (length group1) (length group2))
@@ -39,23 +39,29 @@
       (--each group2 (advent/put set it t))
       (length (--filter (advent/get set it) group1)))))
 
-(defun day19/visible-beacon? (point)
-  point)
+(defun day19/scanners-overlap? (scanner-a scanner-b)
+  (let ((dx (abs (- (car scanner-a) (car scanner-b))))
+        (dy (abs (- (elt scanner-a 1) (elt scanner-b 1))))
+        (dz (abs (- (elt scanner-a 2) (elt scanner-b 2)))))
+    (and (< dx 2000)
+         (< dy 2000)
+         (< dz 2000))))
 
-(defun day19/two-beacons-match-for-translation? (ref other translation)
+(defun day19/two-beacons-match-for-translation? (ref-scanner ref other translation)
   "Returns the translation if it results in at least 12 matches between ref and other or more"
-  (let ((transformed-points (--map (apply translation it) other)))
-    (if (>= (length ref) 12) ;; if I have reduce ref so much, forget about it!
-        (let ((groups (-split-at 10 (-filter #'day19/visible-beacon? transformed-points))))
-          (let ((primary-group (cadr groups))
-                (secondary-group (car groups)))
-            (if (>= (length primary-group) 2)
-                (let ((primary-matches (day19/count-matches ref primary-group)))
-                  (unless (or (< primary-matches 2)
-                              ;; don't bother if the secondary group + 2 can't make to 12
-                              (< (+ primary-matches (length secondary-group)) 12))
-                    (let ((all-matches (+ primary-matches (day19/count-matches ref secondary-group))))
-                      (and (>= all-matches 12) transformed-points))))))))))
+  (let ((transformed-points (--map (apply translation it) other))
+        (candidate-scanner (apply translation '(0 0 0))))
+    (if (day19/scanners-overlap? candidate-scanner ref-scanner)
+     (let ((groups (-split-at 10 transformed-points)))
+       (let ((primary-group (cadr groups))
+             (secondary-group (car groups)))
+         (if (>= (length primary-group) 2)
+             (let ((primary-matches (day19/count-matches ref primary-group)))
+               (unless (or (< primary-matches 2)
+                           ;; don't bother if the secondary group + 2 can't make to 12
+                           (< (+ primary-matches (length secondary-group)) 12))
+                 (let ((all-matches (+ primary-matches (day19/count-matches ref secondary-group))))
+                   (and (>= all-matches 12) (list candidate-scanner transformed-points)))))))))))
 
 (defun day19/create-translation (dest src)
   "Creates a transform that turns src point into dst points
@@ -69,10 +75,7 @@ Can be used to move a point from the 'other' set to the 'reference' set"
             (+ y dy)
             (+ z dz)))))
 
-(defun day19/filter-ref (ref inverse-translation)
-  (--filter (day19/visible-beacon? (apply inverse-translation it)) ref))
-
-(defun day19/oriented-beacons--match? (ref other)
+(defun day19/oriented-beacons--match? (ref-scanner ref other)
   "Given the two groups of beacons in the same orientation, return the transform if there is a match"
   (let ((destination-points ref)
         (result))
@@ -84,18 +87,18 @@ Can be used to move a point from the 'other' set to the 'reference' set"
         (while (and (not result) source-points)
           (let* ((src-point (pop source-points))
                  (translation (day19/create-translation dst-point src-point))
-                 (validated-reference (day19/filter-ref ref (day19/create-translation src-point dst-point))))
-            (setq result (day19/two-beacons-match-for-translation? validated-reference other translation))))))
+                 (validated-reference ref))
+            (setq result (day19/two-beacons-match-for-translation? ref-scanner validated-reference other translation))))))
     result))
 
-(defun day19/two-scanners-match? (ref other)
+(defun day19/two-scanners-match? (ref-scanner ref other)
   "Returns the other points in the new reference if there is a match"
   (let ((remaining-transforms day19/spatial-transforms)
         (result))
     (while (and (not result) remaining-transforms)
       (let* ((current-rotation (pop remaining-transforms))
              (rotated-other (--map (apply current-rotation it) other)))
-        (setq result (day19/oriented-beacons--match? ref rotated-other))))
+        (setq result (day19/oriented-beacons--match? ref-scanner ref rotated-other))))
     result))
 
 (defun day19/read-coordinate (line)
@@ -106,7 +109,6 @@ Can be used to move a point from the 'other' set to the 'reference' set"
   (string-to-number (elt (split-string line " " t) 2)))
 
 (defun day19/read-scan (lines)
-  ;;(print (format "Reading probe '%d'" (day19/read-title (car lines))))
   (-map #'day19/read-coordinate (cdr lines)))
 
 (defun day19/read-scans (blocks)
@@ -122,8 +124,8 @@ Can be used to move a point from the 'other' set to the 'reference' set"
                                  new-size
                                  (- new-size initial-size))))))
 
-(defun day19/add-converted-points (all-points ref-scan other-scans)
-  (day19/debug-print (format "Checking %s vs %s" (car ref-scan) (-map #'car other-scans)))
+(defun day19/add-converted-points (all-points all-scanners ref-scanner ref-scan other-scans)
+  (day19/debug-print (format "Checking %s vs %s (ref-scanner: %s)" (car ref-scan) (-map #'car other-scans) ref-scanner))
   (if other-scans
       (let ((branches)
          (remainders))
@@ -131,36 +133,58 @@ Can be used to move a point from the 'other' set to the 'reference' set"
        (let* ((current-element (pop other-scans))
               (current-index (car current-element))
               (current-data (cdr current-element)))
-         (let ((new-set-or-nil (day19/two-scanners-match? (cdr ref-scan) current-data)))
-           (if (not new-set-or-nil)
+         (let ((scanner-and-new-set-or-nil (day19/two-scanners-match? ref-scanner (cdr ref-scan) current-data)))
+           (if (not scanner-and-new-set-or-nil)
                (push current-element remainders)
-             (progn
-               (day19/accumulate-points all-points (cons current-index new-set-or-nil))
-               (push (cons current-index new-set-or-nil) branches))))))
-     (day19/debug-print (format "Branches: %s" (-map #'car branches)))
-     (day19/debug-print (format "To Check: %s" (-map #'car remainders)))
+             (let ((scanner (car scanner-and-new-set-or-nil))
+                   (converted-beacons (cadr scanner-and-new-set-or-nil)))
+               (day19/accumulate-points all-points (cons current-index converted-beacons))
+               (advent/put all-scanners scanner t)               
+               (push (list scanner (cons current-index converted-beacons)) branches))))))
+     (print (format "Branches: %s" (--map (car (cadr it)) branches)))
+     (print (format "To Check: %s" (--map (car it) remainders)))
      ;; Remove
      (setq other-scans :invalid)
-     (let ((new-remainders (--reduce-from (day19/add-converted-points all-points it acc) remainders  branches)))
+     (let ((new-remainders (--reduce-from (day19/add-converted-points all-points all-scanners (car it) (cadr it) acc) remainders  branches)))
        new-remainders))))
 
 (defun day19/find-all-connections (scans)
   "Returns a list of pairs of transforms"
   (let* ((n-scans (length scans))
          (indexed-scans (--map-indexed (cons it-index it) scans))
-        (all-points (advent/table))
-        (0-scan (car indexed-scans)))
+         (all-points (advent/table))
+         (all-scanners (advent/table))
+         (0-scan (car indexed-scans))
+         (0-scanner (list 0 0 0)))
     (day19/accumulate-points all-points 0-scan)
-    (day19/add-converted-points all-points 0-scan (rest indexed-scans))
-    (print (format "found %d points"(hash-table-count all-points)))
-    (sort (advent/-map-hash all-points it-key) (lambda (a b) (< (car a) (car b))))))
+    (advent/put all-scanners 0-scanner t)
+    (day19/add-converted-points all-points all-scanners 0-scanner 0-scan (rest indexed-scans))
+    (list :beacons
+          (sort (advent/-map-hash all-points it-key) (lambda (a b) (< (car a) (car b))))
+          :scanners
+          (advent/-map-hash all-scanners it-key))))
 
 (defun day19/part-1 (blocks)
   (length
-   (day19/find-all-connections (day19/read-scans blocks))))
+   (plist-get (day19/find-all-connections (day19/read-scans blocks)) :beacons)))
 
-(defun day19/part-2 (lines)
-  (error "Not yet implemented"))
+(defun day19/manhattan (a b)
+  (+ (abs (- (car a) (car b)))
+     (abs (- (elt a 1) (elt b 1)))
+     (abs (- (elt a 2) (elt b 2)))))
+
+
+(defun day19/max-manhattan (scanners)
+  (let ((distances))
+    (loop for i in scanners do
+          (loop for j in scanners do
+                (if (not (equal i j))
+                    (push (day19/manhattan i j) distances))))
+    (apply #'max distances)))
+
+
+(defun day19/part-2 (blocks)
+  (day19/max-manhattan (plist-get (day19/find-all-connections (day19/read-scans blocks)) :scanners)))
 
 (provide 'day19)
 
