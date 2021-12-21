536dd67614d83aacd120d61beaf6c29979fc9cbe
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-21 10:11:35 +0100

    Read problem data

--- day21.el
+++ day21.el
@@ -1,6 +1,13 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defun day21/read-start-pos (line)
+  (string-to-number (elt (split-string line) 4)))
+
+(defun day21/read-input (lines)
+  (vector (day21/read-start-pos (car lines))
+          (day21/read-start-pos (cadr lines))))
+
 (defun day21/part-1 (lines)
   (error "Not yet implemented"))
 
@@ -8,3 +15,6 @@
   (error "Not yet implemented"))
 
 (provide 'day21)
+
+(setq example (day21/read-input(advent/read-problem-lines 21 :example)))
+(setq problem (day21/read-input(advent/read-problem-lines 21 :problem)))
