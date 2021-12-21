469a011898e7cbc0a81ab7bad479b0cfda1ce6a3
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-21 10:20:09 +0100

    WIP for a game move

--- day21.el
+++ day21.el
@@ -5,8 +5,27 @@
   (string-to-number (elt (split-string line) 4)))
 
 (defun day21/read-input (lines)
-  (vector (day21/read-start-pos (car lines))
-          (day21/read-start-pos (cadr lines))))
+  (vector (vector (day21/read-start-pos (car lines)) 0)
+          (vector (day21/read-start-pos (cadr lines)) 0)))
+
+(defun day21/create-det-dice ()
+  (-partition 3 (-flatten (-repeat 3 (number-sequence 1 100)))))
+
+(defun day21/next-det-roll (state)
+  (list (car state)
+        (let ((new-state (cdr state)))
+          (or new-state (day21/create-det-dice)))))
+
+(defun day21/move (position points)
+  "Return the next position after a roll"
+  (1+ (mod (+ (1- position) points) 10)))
+
+(defun day21/move-player (position dice-state)
+  (day21/))
+
+(defun day21/game-step (players dice-state dice-rolls)
+
+  )
 
 (defun day21/part-1 (lines)
   (error "Not yet implemented"))
