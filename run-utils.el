089a77c20c2927b71425a7ec97be405918c5ab45
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-21 20:51:59 +0100

    Improve the Makefile

--- run-utils.el
+++ run-utils.el
@@ -8,8 +8,14 @@
                    (directory-files "."))
          #'string>)))
 
-(defun run-last-test ()
-  (with-temp-buffer    
-    (insert-file (last-test))
+(defun run-test-file (test-file)
+    (with-temp-buffer    
+    (insert-file test-file)
     (eval-buffer)
     (buttercup-run)))
+
+(defun run-last-test ()
+  (run-test-file (last-test)))
+
+(defun run-test (number)
+  (run-test-file (format "day%2.2d-test.el" number)))
