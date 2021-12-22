4ac7959594759e89ce0fa24856918285f8b82495
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-22 11:49:40 +0100

    Read the data

--- day22-test.el
+++ day22-test.el
@@ -1,15 +1,15 @@
 (require 'day22)
 (require 'buttercup)
 
-(defvar small-example (list "on x=10..12,y=10..12,z=10..12"
-                            "on x=11..13,y=11..13,z=11..13"
-                            "off x=9..11,y=9..11,z=9..11"
-                            "on x=10..10,y=10..10,z=10..10"))
+(defvar day22-test/small-example (list "on x=10..12,y=10..12,z=10..12"
+                                       "on x=11..13,y=11..13,z=11..13"
+                                       "off x=9..11,y=9..11,z=9..11"
+                                       "on x=10..10,y=10..10,z=10..10"))
 
 (describe "Day 22"
   (describe "part 1"
     (it "replicates the small example"
-            (expect (day22/part-1 small-example) :to-be 39 ))
+            (expect (day22/part-1 day22-test/small-example) :to-be 39 ))
     (it "replicates the example"
       (expect (day22/part-1 (advent/read-problem-lines 22 :example)) :to-be 590784 ))
     (xit "solves the problem"
--- day22.el
+++ day22.el
@@ -1,6 +1,26 @@
 (require 'dash)
 (require 'advent-utils)
 
+(defun day22/read-interval (token)
+  (let ((as-list (-map #'string-to-number
+                       (split-string (cadr (split-string token "=" t)) "[.]+" t))))
+    (cons (car as-list)
+          (cadr as-list))))
+
+(defun day22/read-state (token)
+  (cond
+   ((string= token "on") 1)
+   ((string= token "off") 0)
+   (t (error "Unexpected token"))))
+
+(defun day22/read-step (line)
+  (let* ((state-coords (split-string line " " t))
+         (coords (-map #'day22/read-interval (split-string (cadr state-coords) "," t))))
+    (list coords (day22/read-state (car state-coords)))))
+
+(defun day22/read-steps (lines)
+  (-map #'day22/read-step lines))
+
 (defun day22/part-1 (lines)
   (error "Not yet implemented"))
 
@@ -8,3 +28,6 @@
   (error "Not yet implemented"))
 
 (provide 'day22)
+
+(setq example (day22/read-steps (advent/read-problem-lines 22 :example)))
+(setq problem (day22/read-steps (advent/read-problem-lines 22 :problem)))
