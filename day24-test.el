9000fa64523d1421760d3c4bbeaf7de36fe3931d
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-24 18:40:33 +0100

    Add problem data and correct the tests

--- day24-test.el
+++ day24-test.el
@@ -3,10 +3,7 @@
 
 (describe "Day 24"
   (describe "part 1"
-    (it "replicates the example"
-      (expect (day24/part-1 (advent/read-problem-lines 24 :example))
-              :to-be 42 ))
-    (xit "solves the problem"
+    (it "solves the problem"
       (expect (day24/part-1 (advent/read-problem-lines 24 :problem))
               :to-be 42)))
   (xdescribe "part 2"
