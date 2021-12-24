00779d3aa0424be52fe38130f4feaa186ec3f316
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-24 03:09:43 +0100

    Add boilerplate for part 2

--- day23-test.el
+++ day23-test.el
@@ -2,14 +2,14 @@
 (require 'buttercup)
 
 (describe "Day 23"
-  (describe "part 1"
+  (xdescribe "part 1"
     (it "replicates the example"
       (expect (day23/part-1 (advent/read-problem-lines 23 :example 1))
               :to-be 12521))
     (it "solves the problem"
       (expect (day23/part-1 (advent/read-problem-lines 23 :problem 1))
               :to-be 16157)))
-  (xdescribe "part 2"
+  (describe "part 2"
     (it "replicates the example"
       (expect (day23/part-2 (advent/read-problem-lines 23 :example 2))
               :to-be 44169))
