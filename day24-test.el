f4289f5b3e7f6cba4b232817c282f18941776e2f
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2021-12-24 11:14:02 +0100

    Add boilerplate code for day 24

---
+++ day24-test.el
@@ -0,0 +1,18 @@
+(require 'day24)
+(require 'buttercup)
+
+(describe "Day 24"
+  (describe "part 1"
+    (it "replicates the example"
+      (expect (day24/part-1 (advent/read-problem-lines 24 :example))
+              :to-be 42 ))
+    (xit "solves the problem"
+      (expect (day24/part-1 (advent/read-problem-lines 24 :problem))
+              :to-be 42)))
+  (xdescribe "part 2"
+    (it "replicates the example"
+      (expect (day24/part-2 (advent/read-problem-lines 24 :example))
+              :to-be 42))
+    (xit "solves the problem"
+      (expect (day24/part-2 (advent/read-problem-lines 24 :problem))
+              :to-be 42))))
---
+++ day24.el
@@ -0,0 +1,10 @@
+(require 'dash)
+(require 'advent-utils)
+
+(defun day24/part-1 (lines)
+  (error "Not yet implemented"))
+
+(defun day24/part-2 (lines)
+  (error "Not yet implemented"))
+
+(provide 'day24)
